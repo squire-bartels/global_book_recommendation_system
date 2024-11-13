392cf6d12e084baf69440098fb8bdfc836af922d
Charles Bartels Squire <149335807+squire-bartels@users.noreply.github.com>
2024-11-13 00:14:24 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 # Updated variables
-REPO_NAME = "ML Based Books Recommender System"
+REPO_NAME = "Global Book Recommendation System"
 AUTHOR_USER_NAME = "Charles Bartels Squire"
-SRC_REPO = "books_recommender"
-LIST_OF_REQUIREMENTS = []
+SRC_REPO = "global_book_recommender"
+LIST_OF_REQUIREMENTS = [
+    "streamlit",       # Streamlit for web app
+    "numpy",           # NumPy for numerical operations
+    "pandas",          # Pandas for data manipulation
+    "scikit-learn",    # Scikit-learn for machine learning algorithms
+    "-e .",
+    "nltk",
+    "PyYAML"
+]
 
 setup(
     name=SRC_REPO,
@@ -16,12 +24,9 @@ setup(
     description="A small local package for ML-based books recommendations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/squire-bartels/ML-Based-Book-Recommender-System",  
+    url="https://github.com/squire-bartels/global_book_recommendation_system",
     author_email="charlesbartelssquire@gmail.com",
     packages=find_packages(),
-    python_requires=" 3.13.0",
+    python_requires=">=3.7, <3.12",  # Python version range
     install_requires=LIST_OF_REQUIREMENTS
 )
-
-
-
